43f346bd7584449e55566998a8d9e311a3fc71df
bastienpog <147522260+bastienpog@users.noreply.github.com>
2025-01-26 12:44:34 +0100

    Add filter button with category dropdown menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,51 @@
 "use client";
 
-import { Sidebar, SidebarHeader} from '@/components/ui/sidebar';
-import { Input } from '@/components/ui/input';
-import { DropdownMenu } from '@/components/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sidebar, SidebarHeader, SidebarMenuButton } from "@/components/ui/sidebar";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+import { Filter } from "lucide-react";
 import { useState } from "react";
 
-
 const Page: React.FC = (): React.JSX.Element => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
+  type Checked = DropdownMenuCheckboxItemProps["checked"];
+
   const snippets = [
-        { title: "Snippet 1", language: "JavaScript", description: "A basic JS snippet", content: 'console.log("Hello, world!");' },
-        { title: "Snippet 2", language: "Python", description: "A basic Python snippet", content: 'print("Hello, world!")' },
-        { title: "Snippet 3", language: "Java", description: "A basic Java snippet", content: 'System.out.println("Hello, world!");' },
-        { title: "Snippet 4", language: "Ruby", description: "A basic Ruby snippet", content: 'puts "Hello, world!"' },
-        { title: "Snippet 5", language: "Go", description: "A basic Go snippet", content: 'fmt.Println("Hello, world!")' },
-        { title: "Snippet 6", language: "C", description: "A basic C snippet", content: 'printf("Hello, world!");' },
-        { title: "Snippet 7", language: "PHP", description: "A basic PHP snippet", content: 'echo "Hello, world!";' },
-        { title: "Snippet 8", language: "Swift", description: "A basic Swift snippet", content: 'print("Hello, world!")' },
-        { title: "Snippet 9", language: "TypeScript", description: "A basic TypeScript snippet", content: 'console.log("Hello, world!");' },
+    { title: "Snippet 1", language: "JavaScript", description: "A basic JS snippet", categories: "Front" },
+    { title: "Snippet 2", language: "Python", description: "A basic Python snippet", categories: "Front" },
+    { title: "Snippet 3", language: "Java", description: "A basic Java snippet", categories: "Front" },
+    { title: "Snippet 4", language: "Ruby", description: "A basic Ruby snippet", categories: "Back" },
+    { title: "Snippet 5", language: "Go", description: "A basic Go snippet", categories: "Back" },
+    { title: "Snippet 6", language: "C", description: "A basic C snippet", categories: "Back" },
+    { title: "Snippet 7", language: "PHP", description: "A basic PHP snippet", categories: "Memo" },
+    { title: "Snippet 8", language: "Swift", description: "A basic Swift snippet", categories: "Cheat" },
+    { title: "Snippet 9", language: "TypeScript", description: "A basic TypeScript snippet", categories: "Start" },
   ];
+
+  const uniqueCategories = Array.from(new Set(snippets.map((snippet) => snippet.categories)));
 
   return (
     <Sidebar>
       <SidebarHeader className="border-b p-2">
-      <Input 
-        type="text"
-        placeholder={"Search"}
-        value={query}
-        onChange={handleInputChange}
-        className="w-full p-2 "
-      />
-      <DropdownMenu></DropdownMenu>
+        <Input type="text" placeholder={"Search"} value={query} onChange={handleInputChange} className="w-full p-2 " />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton>
+              <Filter />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+            {uniqueCategories.map((category) => (
+              <DropdownMenuCheckboxItem key={category}>{category}</DropdownMenuCheckboxItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </SidebarHeader>
     </Sidebar>
   );
