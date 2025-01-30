a6f67e4412337f9d7a0148111d38213b47520949
Martin <martin.dinahet@proton.me>
2025-01-30 18:50:37 +0100

    hooked up dynamic pages to snippets provider, installed syntax highlighting library, implemented syntax highlighting in snippet

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+"use client";
 
-interface Props {
-  params: Promise<{
-    id: string;
-  }>;
-}
+import { useParams } from "next/navigation";
+import { useSnippetsContext } from "@/components/custom/SnippetsProvider";
+import { Snippet } from "@/lib/entities/Snippet";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
-const Page = async ({ params }: Props): Promise<React.JSX.Element> => {
-  const { id } = await params;
+const Page: React.FC = (): React.JSX.Element => {
+  const { snippets, uniqueTags, error, loading } = useSnippetsContext();
+  const { id } = useParams();
+  const snippet: Snippet | undefined = snippets.find((snippet) => snippet.id === id);
+  if (!snippet) {
+    return <div>Snippet not found</div>;
+  }
 
   return (
     <>
-      <h1>{id}</h1>
+      <h1>{snippet.title}</h1>
+      <SyntaxHighlighter language={snippet.language}>{snippet.content}</SyntaxHighlighter>
     </>
   );
 };
