5d99f9200665d25d34aa9e738ab08de1a6e95263
Martin <martin.dinahet@proton.me>
2025-02-08 13:51:56 +0100

    removed old files

--- src/lib/database.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata";
-import { AppDataSource } from "./data-source";
-
-export async function InitializeDatabase() {
-  if (!AppDataSource.isInitialized) {
-    await AppDataSource.initialize();
-  }
-  return AppDataSource;
-}
